32425fb11962215752f7adb6344aec531c40073b
flvSantos15 <flvsantos300@gmail.com>
2025-07-12 10:28:36 -0300

    chore: add video test

--- encoder/domain/video.go
+++ encoder/domain/video.go
@@ -1,10 +1,34 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+)
 
 type Video struct {
-	ID         string
-	ResourceID string
-	FilePath   string
-	Created    time.Time
+	ID         string `valid:"uuid"`
+	ResourceID string `valid:"notnull"`
+	FilePath   string `valid:"notnull"`
+	CreatedAt    time.Time `valid:"-"`
+}
+
+// Esse codigo vai rodar antes de todo mundo
+func init() {
+	// essa parte vai exigir que tudo seja true
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+func NewVideo() *Video {
+	return &Video{}
+}
+
+func (video *Video) Validate() error {
+	_, err := govalidator.ValidateStruct(video)
+	
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
