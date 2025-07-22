db128087cb5a3ad861f7f95340a1f1e1251dfbde
flvSantos15 <flvsantos300@gmail.com>
2025-07-21 23:29:15 -0300

    chore: add video service

---
+++ encoder/application/services/video_service.go
@@ -0,0 +1,60 @@
+package services
+
+import (
+	"context"
+	"enconder/application/repositories"
+	"enconder/domain"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"cloud.google.com/go/storage"
+)
+
+type VideoService struct {
+	Video *domain.Video
+	VideoRepository repositories.VideoRepository
+}
+
+func NewVideoService() VideoService {
+	return VideoService{}
+}
+
+func (service *VideoService) Download(bucketName string) error {
+	ctx := context.Background()
+	
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	bucket := client.Bucket(bucketName)
+	object := bucket.Object(service.Video.FilePath)
+
+	reader, err := object.NewReader(ctx)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	body, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil
+	}
+
+	file, err := os.Create(os.Getenv("localStoragePath") + "/" + service.Video.ID + ".mp4")
+	if err != nil {
+		return nil
+	}
+
+	_, err = file.Write(body)
+	if err != nil {
+		return nil
+	}
+
+	defer file.Close()
+
+	log.Printf("video %v has been stored", service.Video.ID)
+
+	return nil
+}
